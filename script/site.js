dffbfa3fa639d0d27319baba0c26306d857c071a
Meixl <300093803@fvtc.edu>
2025-01-16 13:22:19 -0600

    first commit

---
+++ script/site.js
@@ -0,0 +1,23 @@
+const now = (new Date()).getSeconds()
+
+const p =  document.querySelector('p')
+const h1 = document.querySelector('h1')
+console.log(h1);
+
+
+
+setTimeout(() => {
+
+p.textContent = 'Genral Kenobi!' + seconds
+}, 2000)
+
+setTimeout(()=> {
+    if (second % 2 ==1)
+    {
+    p.style.color = '#990000'
+    }
+    else
+    {
+        p.style.color = '#00CC00'
+    }
+},3000)
